c42d84c2bbb7419f20189b9bef51130e360d2c66
MikaAK <mikakalathil@gmail.com>
2014-06-22 16:55:15 -0700

    Finish adding uuid Auth

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,21 +1,13 @@
 class CohortsController < ApplicationController
 
   def index
-    redirect_to show, status: 'Access Forbidden' unless @visitor.admin?
     @cohorts = Cohort.all
     render_layout
   end
 
   def show
-    origin_url unless @visitor.admin?
-    id = @pid || params[:id]
-    binding.pry
-    @cohort = Cohort.find id
+    @cohort = Cohort.find params[:id]
     @students = @cohort.students
     render_layout
-  end
-
-  def origin_url
-    @pid = @pid || params[:id]
   end
 end
