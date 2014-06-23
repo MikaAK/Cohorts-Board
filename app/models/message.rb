7c154ecc32ef4cc3a107709453b2722c94017462
MikaAK <mikakalathil@gmail.com>
2014-06-22 20:08:35 -0700

    Add validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message
   extend ActiveModel::Naming
   attr_accessor :content, :name, :email, :title, :cohort_id
 
+  validates :email, email: true
+  validates :cohort_id, numericality: { greater_than: 0, less_than_or_equal_to: Cohort.last.id }
 
   def initialize(attributes = {})
     attributes.each do |name, value|
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,7 +1,9 @@
 class Visitor < ActiveRecord::Base
 
+  validates :phone_number, format: /\Z(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?\z/i
+  validates :email, email: true
+
   def admin?
     self.uuid == ENV['ADMIN_UUID']
   end
-  
 end
