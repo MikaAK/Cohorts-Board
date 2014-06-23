66222ab4cc89da8b7e2e5ab08d1e6439c4c95abe
MikaAK <mikakalathil@gmail.com>
2014-06-22 21:21:11 -0700

    Add sidebar style

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,11 @@
 class VisitorsController < ApplicationController
   def new
     @message = Message.new(message_params)
+    binding.pry
     if @message.valid?
       redirect_to cohort_path(params[:cohort_id]), notice: "Message sent successfully"
     else
+      flash[:error] = "Message send unsuccessfull"
       redirect_to cohort_path(params[:cohort_id])
     end
   end
