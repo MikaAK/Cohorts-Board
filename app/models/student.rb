981e5212aa933dd9bdaa7634e02fa51355f3a0e5
MikaAK <mikakalathil@gmail.com>
2014-07-01 09:45:05 -0700

    fix styling on code and try morphin buttons

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,12 +6,24 @@ class Student < ActiveRecord::Base
   mount_uploader :avatar, StudentAvatarUploader
   mount_uploader :resume, StudentResumeUploader
 
-  validates :first_name, length: { minimum: 0, maxiumum: 50 }
-  validates :cohort, presence: true
-  validates :email, email: true
-  validates :short_bio, length: { minimum: 25, maximum: 250 }, allow_blank: true, if: :registered?
-  validates :last_name, length: { minimum: 0, maxiumum: 50 }, if: :registered?
-  validates :personal_website_url, url: true, allow_blank: true, if: :registered?
+  validates :first_name,
+    length: { minimum: 0, maxiumum: 50 }
+    
+  validates :cohort,
+    presence: true
+
+  validates :email,
+    email: true
+
+  validates :short_bio,
+    length: { minimum: 25, maximum: 250 },
+    allow_blank: true, if: :registered?
+
+  validates :last_name,
+    length: { minimum: 0, maxiumum: 50 }, if: :registered?
+
+  validates :personal_website_url,
+    url: true, allow_blank: true, if: :registered?
 
 
   def full_name
--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Cohortboard
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w( jpanelmenu.min.js employers.js admin.js welcome.js morphing_buttons.min.js modernizer.min.js )
+    config.assets.precompile += %w( jpanelmenu.min.js employers.js admin.js welcome.js uiMorphingButton_fixed.js modernizer.min.js classie.js)
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
